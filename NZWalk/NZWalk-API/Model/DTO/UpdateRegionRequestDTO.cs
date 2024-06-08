19a33ec4063e3a1f7184d23b34d1f29fcde37f3d
Shubham Kumar <2090944@cognizant.com>
2024-06-08 16:52:40 +0530

    Day_6. Adding Model Validations To Endpoints

--- NZWalk/NZWalk-API/Model/DTO/UpdateRegionRequestDTO.cs
+++ NZWalk/NZWalk-API/Model/DTO/UpdateRegionRequestDTO.cs
@@ -1,8 +1,16 @@
-﻿namespace NZWalk_API.Model.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalk_API.Model.DTO
 {
     public class UpdateRegionRequestDTO
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Name has to be minimum 3 Character")]
+        [MaxLength(30, ErrorMessage = "Name has to be Maximum 30 Character")]
         public string Name { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "Code has to be minimum 3 Character")]
+        [MaxLength(4, ErrorMessage = "Code has to be Maximum 4 Character")]
         public string Code { get; set; }
         public string? RegionImageUrl { get; set; }
     }
--- NZWalk/NZWalk-API/Model/DTO/UpdateWalkRequestDTO.cs
+++ NZWalk/NZWalk-API/Model/DTO/UpdateWalkRequestDTO.cs
@@ -1,13 +1,21 @@
-﻿namespace NZWalk_API.Model.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalk_API.Model.DTO
 {
     public class UpdateWalkRequestDTO
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
-
+        [Required]
         public double LengthInKm { get; set; }
+        [Required]
+        [Range(1, 100)]
         public string? WalkImageUrl { get; set; }
+        [Required]
         public Guid DiffucaltyId { get; set; }
+        [Required]
         public Guid RegionId { get; set; }
 
 
