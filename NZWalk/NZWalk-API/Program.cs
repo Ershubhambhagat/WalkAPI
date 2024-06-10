5dc1c3cafdf72b5e916c3dbe8ba054bcab72f8ef
Shubham Kumar <2090944@cognizant.com>
2024-06-10 18:20:51 +0530

    Day_8 || Setting Up Authentication - Install Nuget Packages

--- NZWalk/NZWalk-API/Program.cs
+++ NZWalk/NZWalk-API/Program.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NZWalk_API.Data;
 using NZWalk_API.Mappings;
 using NZWalk_API.Repositories;
 using NZWalk_API.Repositories.Interface;
 using NZWalk_API.Repositories.Walk_Repository;
 using NZWalk_API.Repositories.Walk_Repository.Interface;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +24,18 @@ builder.Services.AddScoped<IWalkRepository, WalkRepository>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer=true
+        ,ValidateAudience=true,
+        ValidateLifetime=true,
+        ValidateIssuerSigningKey=true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration.["Jwt:Audience"],
+        IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        
+    });
 
 
 var app = builder.Build();
@@ -34,6 +48,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
