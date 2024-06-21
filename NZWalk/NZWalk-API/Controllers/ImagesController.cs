9377c9547f411eabd89fa1cf9ea0e26508630f5f
Shubham Kumar <2090944@cognizant.com>
2024-06-21 17:36:53 +0530

    Day_10 || >   New Controller and Action Method for Image Upload.

---
+++ NZWalk/NZWalk-API/Controllers/ImagesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalk_API.Model.DTO.ImageDTOs;
+
+namespace NZWalk_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+
+
+        #region Uplode
+        [HttpPost]
+        public async Task<IActionResult> Uplode([FromForm] ImageUploadRequestDto request)
+        {
+
+            ValidateFileUpload(request);
+            if(ModelState.IsValid)
+            {
+                //Uplodeing Image 
+                return Ok();
+
+
+            }
+            return BadRequest(ModelState);
+        }
+
+
+        #endregion
+
+
+        #region ValidateFileUpload
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtension = new string[]
+            {
+               " .jpg",".png"
+            };
+            //If File Extension not belon to this Then
+            if (!allowedExtension.Contains(Path.GetExtension(request.FileName)))
+            {
+                //Give error 
+                ModelState.AddModelError("File", "Unsupported File Extension");
+
+            }
+            // if File is more then 10 MB then
+            if (request.File.Length > 10485760)
+            {
+                //Give error 
+                ModelState.AddModelError("File", "File Size More then 10 MB, Uplode Smaller File");
+            }
+        }
+
+
+
+        #endregion
+
+
+        #region MyRegion
+
+        #endregion
+
+
+        #region MyRegion
+
+        #endregion
+    }
+}
---
+++ NZWalk/NZWalk-API/Model/DTO/ImageDTOs/ImageUploadRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace NZWalk_API.Model.DTO.ImageDTOs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageUploadRequestDto : ControllerBase
+    {
+        [Required]
+        public IFormFile File { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        public string? FileDescription { get; set; }
+    }
+}
