a892d89309cf8a0661cad05f8083f228234943d6
Shubham Kumar <2090944@cognizant.com>
2024-06-21 11:01:48 +0530

    Day_10 || > Create Auth Controller and Register Action Method

---
+++ NZWalk/NZWalk-API/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalk_API.Model.DTO.AuthDTOs;
+
+namespace NZWalk_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private const string Error = "Somthing Went Wrong";
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public static string Error1 => Error;
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody]RegisterRequestDto registerRequestDto)
+        {
+            var IdentityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+
+            };
+            var identityResult =await _userManager.CreateAsync(IdentityUser, registerRequestDto.Password);
+            if(identityResult.Succeeded)
+            {
+                if (registerRequestDto.Roles is not null && registerRequestDto.Roles.Any())
+                {
+                    //Add roles to user
+                    identityResult = await _userManager.AddToRoleAsync(IdentityUser, registerRequestDto.Roles);
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User Was Created , Login...");
+                    }
+                }
+            }
+            return BadRequest(Error1);
+        }
+    }
+}
