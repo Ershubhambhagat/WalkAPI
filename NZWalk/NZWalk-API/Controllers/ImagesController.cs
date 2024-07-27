81572dfc5220c630f7a0094edffded0a51dda467
Shubham Kumar <2090944@cognizant.com>
2024-07-27 12:21:38 +0530

    Day_11 || >New image Controller and Action Method for Image Upload

---
+++ NZWalk/NZWalk-API/Controllers/ImagesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.Mvc;
+using NZWalk_API.Model.DTO.ImageDTOs;
+
+namespace NZWalk_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Uplode")]
+        public async Task<IActionResult> upload([FromForm]ImageUplodeRequestDTOs requestImage)
+        {
+            ValidateFileUpload(requestImage);
+            if(ModelState.IsValid)
+            {
+                //uplode file path in database
+
+            }
+            return BadRequest(ModelState);
+
+        }
+        //validate image Extension File and size
+        private void ValidateFileUpload(ImageUplodeRequestDTOs requestImage)
+        {
+            var allowExtension = new string[] { ".jpg",".png"};
+            if (!allowExtension.Contains(Path.GetExtension(requestImage.File.FileName)))
+            {
+                ModelState.AddModelError("File", "FileExtension is not correct , use only JPG and Png file");
+            }
+            if(requestImage.File.Length > 1000000)
+            {
+                ModelState.AddModelError("File", "File Size is more then 1 MB");
+
+
+            }
+        }
+    }
+}
---
+++ NZWalk/NZWalk-API/Model/DTO/ImageDTOs/ImageUplodeRequestDTOs.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace NZWalk_API.Model.DTO.ImageDTOs
+{
+    public class ImageUplodeRequestDTOs
+    {
+        [Required]
+        public IFormFile File { get; set; }
+        [Required]
+        public string  FileName { get; set; }
+        public string?  FileDiscription { get; set; }
+    }
+}
