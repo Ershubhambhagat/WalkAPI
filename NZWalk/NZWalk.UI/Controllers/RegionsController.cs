c94eab54c6cc5364897db18990c23ea3932b6984
Shubham Kumar <2090944@cognizant.com>
2024-07-29 20:04:39 +0530

    Day_12 ||  Setup UI File

---
+++ NZWalk/NZWalk.UI/Controllers/RegionsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalk.UI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Get all the resion From API
+
+
+            return View();
+        }
+    }
+}
